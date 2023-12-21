5fccef894c8010adfa492ccd54f10cab1f817eb4
Descotto <davidescotto@hotmail.com>
2023-12-20 23:19:31 -0800

    flight pattenrs

--- ships.py
+++ ships.py
@@ -12,10 +12,6 @@ class Ship1(Enemy):
         self.speed = 7
         self.hp = 1
         self.bullet_type = 'Shot2'
-        
-
-
-
 
     def get_player_distance_direction(self,player):
         enemy_vec = pygame.math.Vector2(self.rect.center)
@@ -39,7 +35,6 @@ class Ship1(Enemy):
         
         return (distance,direction)
     
-
     def update(self,player):
         self.rect.x += self.speed * self.direction.x
         self.rect.y += self.speed * self.direction.y
@@ -59,16 +54,32 @@ class Ship2(Enemy):
         self.image = self.flip
         self.moving = True
         self.speed = 1
-        self.hp = 5
+        self.hp = 4
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(-64,-96)
         self.bullet_type = 'Shot4'
 
-
-
-
-
-
+    def get_player_distance_direction(self,player):
+        enemy_vec = pygame.math.Vector2(self.rect.center)
+        player_vec = pygame.math.Vector2(player.rect.topright)
+        distance = (player_vec - enemy_vec).magnitude()
+        # enemy_vec.distance_to(player_vec)
+        
+        if distance < 650:
+            self.moving = True
+            direction = (player_vec - enemy_vec).normalize()
+            if player.rect.x < self.rect.x:
+                self.shoot(self,direction)
+        elif distance < 900 and self.moving:
+            direction = pygame.math.Vector2(2.5,0)
+            self.direction = pygame.math.Vector2(direction)
+            
+        else:
+            
+            direction = pygame.math.Vector2(-1,0)
+            self.direction = pygame.math.Vector2(0,0)
+        
+        return (distance,direction)
 
     def update(self,player):
         self.rect.x += self.speed * self.direction.x
@@ -89,35 +100,10 @@ class Ship3(Enemy):
         self.flip = pygame.transform.flip(self.image, True,False)
         self.image = self.flip
         self.moving = True
-        self.speed = 2
+        self.speed = 5
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(-64,-96)
-
-
-    def update(self,player):
-        self.rect.x += self.speed * self.direction.x
-        self.rect.y += self.speed * self.direction.y
-        self.hitbox.center = self.rect.center
-        self.get_player_distance_direction(player)
-        self.trigger_death(self)
-
-
-# =================================================================================================
-        
-
-class Ship4(Enemy):
-    def __init__(self,pos,group,shoot,trigger_death):
-        super().__init__(pos,group,shoot,trigger_death)
-        self.image = pygame.image.load('./Assets/enemies/3/Ship4.png').convert_alpha()
-        self.flip = pygame.transform.flip(self.image, True,False)
-        self.image = self.flip
-        self.moving = True
-        self.speed = 2
-        self.hp = 5
-        self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(-32,-96)
-        self.bullet_type = 'Shot6'
-
+        self.bullet_type = 'Shot3'
 
     def get_player_distance_direction(self,player):
         enemy_vec = pygame.math.Vector2(self.rect.center)
@@ -141,7 +127,52 @@ class Ship4(Enemy):
         
         return (distance,direction)
     
+    def update(self,player):
+        self.rect.x += self.speed * self.direction.x
+        self.rect.y += self.speed * self.direction.y
+        self.hitbox.center = self.rect.center
+        self.get_player_distance_direction(player)
+        self.trigger_death(self)
 
+
+# =================================================================================================
+        
+
+class Ship4(Enemy):
+    def __init__(self,pos,group,shoot,trigger_death):
+        super().__init__(pos,group,shoot,trigger_death)
+        self.image = pygame.image.load('./Assets/enemies/3/Ship4.png').convert_alpha()
+        self.flip = pygame.transform.flip(self.image, True,False)
+        self.image = self.flip
+        self.moving = True
+        self.speed = 2
+        self.hp = 7
+        self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(-32,-96)
+        self.bullet_type = 'Shot6'
+
+    def get_player_distance_direction(self,player):
+        enemy_vec = pygame.math.Vector2(self.rect.center)
+        player_vec = pygame.math.Vector2(player.rect.topright)
+        distance = (player_vec - enemy_vec).magnitude()
+        # enemy_vec.distance_to(player_vec)
+        
+        if distance < 650:
+            self.moving = True
+            direction = (player_vec - enemy_vec).normalize()
+            self.direction = pygame.math.Vector2(2,0)
+        elif distance < 900 and self.moving:
+            direction = pygame.math.Vector2(2.5,0)
+            if player.rect.x < self.rect.x:
+                self.shoot(self,direction)
+            
+        else:
+            
+            direction = pygame.math.Vector2(-1,0)
+            self.direction = pygame.math.Vector2(0,0)
+        
+        return (distance,direction)
+    
     def update(self,player):
         self.rect.x += self.speed * self.direction.x
         self.rect.y += self.speed * self.direction.y
